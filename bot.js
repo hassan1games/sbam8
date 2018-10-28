aadcbf93b1af1bff0f6bd6a0130c324db97e14fc
hassan1games <44328174+hassan1games@users.noreply.github.com>
2018-10-28 09:26:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("505371254606266395")
+let channel =     client.channels.get("506001232561963019")
 setInterval(function() {
 channel.send(` 
 **  
